0018e8397b30d60be411aee9b0fb4d4d810e872b
Flushimoto <njoni.prishtine@gmail.com>
2025-08-20 02:41:46 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'TOILETCOIN',
-  description: 'The Final Flush. Born mid-poop by Satoshi Flushimoto. 1B supply. Utility: none.'
+  description: 'The Final Flush. Born mid-poop by Satoshi Flushimoto. Utility: none.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
